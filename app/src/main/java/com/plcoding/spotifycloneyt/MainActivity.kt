ae998dd6dab80e201bbb53ec7d82149788c306f4
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-06-21 21:16:30 +0200

    -new files

--- app/src/main/java/com/plcoding/spotifycloneyt/MainActivity.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/MainActivity.kt
@@ -3,7 +3,6 @@ package com.plcoding.spotifycloneyt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.RequestManager
-import dagger.hilt.EntryPoint
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
