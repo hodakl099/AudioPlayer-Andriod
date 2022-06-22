4e814504357ca367f31a57fefd9830ffcf4bf38a
Mahmoud Alkateb <mahmoud.alkateb22@gmail.com>
2022-06-22 10:42:07 +0200

    -new files

--- app/src/main/java/com/plcoding/spotifycloneyt/data/entities/other/Constants.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/data/entities/other/Constants.kt
@@ -3,4 +3,7 @@ package com.plcoding.spotifycloneyt.data.entities.other
 object Constants {
 
     val AUDIO_COLLECION = "aduios"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
