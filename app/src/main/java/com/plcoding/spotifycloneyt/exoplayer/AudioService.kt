bbf00539d93dbb9797c1a93a045b409ca4d92b19
Mahmoud Alkawteb <mahmoud.alkateb22@gmail.com>
2022-07-20 20:18:19 +0200

    -new files

--- app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/AudioService.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/AudioService.kt
@@ -1,22 +1,19 @@
 package com.plcoding.spotifycloneyt.exoplayer
-
 import android.app.PendingIntent
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.plcoding.spotifycloneyt.exoplayer.callbacks.AudioPlayerNotificationListener
+import com.plcoding.spotifycloneyt.exoplayer.callbacks.*
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
-private const val SERVICE_TAG = "MusicService"
+private const val SERVICE_TAG = "AudioService"
 
 @AndroidEntryPoint
 class AudioService : MediaBrowserServiceCompat() {
@@ -27,7 +24,10 @@ class AudioService : MediaBrowserServiceCompat() {
     @Inject
     lateinit var exoPlayer: SimpleExoPlayer
 
-    private lateinit var audioNotificationManager: AudioNotificationManager
+    @Inject
+    lateinit var firebaseMusicSource: FirebaseAudioSource
+
+    private lateinit var musicNotificationManager: AudioNotificationManager
 
     private val serviceJob = Job()
     private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
@@ -37,11 +37,17 @@ class AudioService : MediaBrowserServiceCompat() {
 
     var isForegroundService = false
 
+    private var curPlayingSong: MediaMetadataCompat? = null
+
     override fun onCreate() {
         super.onCreate()
+        serviceScope.launch {
+            FirebaseAudioSource.fetchMedisData()
+        }
         val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
             PendingIntent.getActivity(this, 0, it, 0)
         }
+
 
         mediaSession = MediaSessionCompat(this, SERVICE_TAG).apply {
             setSessionActivity(activityIntent)
@@ -50,7 +56,7 @@ class AudioService : MediaBrowserServiceCompat() {
 
         sessionToken = mediaSession.sessionToken
 
-        audioNotificationManager = AudioNotificationManager(
+        musicNotificationManager = AudioNotificationManager(
             this,
             mediaSession.sessionToken,
             AudioPlayerNotificationListener(this)
@@ -58,8 +64,32 @@ class AudioService : MediaBrowserServiceCompat() {
 
         }
 
+        val musicPlaybackPreparer = MusicPlaybackPreparer(firebaseMusicSource) {
+            curPlayingSong = it
+            preparePlayer(
+                firebaseMusicSource.audios,
+                it,
+                true
+            )
+        }
+
         mediaSessionConnector = MediaSessionConnector(mediaSession)
+        mediaSessionConnector.setPlaybackPreparer(musicPlaybackPreparer)
         mediaSessionConnector.setPlayer(exoPlayer)
+
+        exoPlayer.addListener(AudioPlayerEventListener(this))
+        musicNotificationManager.showNotification(exoPlayer)
+    }
+
+    private fun preparePlayer(
+        songs: List<MediaMetadataCompat>,
+        itemToPlay: MediaMetadataCompat?,
+        playNow: Boolean
+    ) {
+        val curSongIndex = if(curPlayingSong == null) 0 else songs.indexOf(itemToPlay)
+        exoPlayer.prepare(firebaseMusicSource.asMediaSource(dataSourceFactory))
+        exoPlayer.seekTo(curSongIndex, 0L)
+        exoPlayer.playWhenReady = playNow
     }
 
     override fun onDestroy() {
